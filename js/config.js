ee3796d03970b791c6f149fda90d02cf03647563
skteminofficial <sekolah-2342@moe-dl.edu.my>
2026-09-05 22:37:49 +0800

    Update version in config.js to 002.4

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.TEMIN_CONFIG = {
   backendUrl: "https://script.google.com/macros/s/AKfycbw5PuZqR9tjvh6yU65gtv01wRF2I82IDtBnckAGsv9ogMn03sV12434NIZHv-wq7NIG/exec",
-  version: "002.3.1"
+  version:'002.4'
 };
